d123dbdd19dc01e673802a7a0d4cbc8ee6047f6f
Slava <slavamonster6@gmail.com>
2024-03-12 20:12:11 +0300

    line

--- script/script.js
+++ script/script.js
@@ -18,18 +18,27 @@ start(cells);
 function start(cells) {
     cells.forEach(element => {
         element.addEventListener('click', function step() {
+            let stepText = document.querySelector('.step span');
+            console.log(stepText);
             if (count % 2 == 0) {
                 element.textContent = "X";
+                stepText.textContent = "O";
             } else {
                 element.textContent = "O";
+                stepText.textContent = "X";
             }
-        
+
+            let select_size = document.querySelector('.select-size');
+            let value = select_size.value;
+            if (value == 'random') random_size ();
+
             let victory = isVictory(cells);
         
             if (victory) {
                 select(victory);
+                line(victory);
                 alerts(`Выйграл ${element.textContent}`);
-                reload();
+                //reload();
             } else if (count == 8){
                 alerts(`Ничья!`);
                 reload();
@@ -44,12 +53,12 @@ function start(cells) {
 
 function isVictory(cells) {
 	let combs = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
+		[0, 1, 2],// vert
+		[3, 4, 5],//vert
+		[6, 7, 8],//vert
+		[0, 3, 6],//hor
+		[1, 4, 7],//hor
+		[2, 5, 8],//hor
 		[0, 4, 8],
 		[2, 4, 6],
 	];
@@ -84,3 +93,29 @@ select_size.addEventListener('change', function () {
         table.classList.remove('field__min');
     }
 });
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * max + min);
+  }
+
+function random_size () {
+    let table_size = document.querySelectorAll('.field td');
+    let size = getRandomInt(50, 150);
+
+    table_size.forEach(e => {
+        e.style.width = size + 'px';
+        e.style.height = size + 'px';
+        e.style.fontSize = size * 0.6 + 'px';
+    });
+}
+
+function line(arr) {
+    let element = cells[arr[0]];
+    element.classList.add('line');
+
+    let number = "" + arr[0] + arr[1] + arr[2];
+
+    if (number == "036" || number == "147" || number == "258")  element.classList.add('horizon');
+    else if (number == "048") element.classList.add('diagonal-main'); 
+    else if (number == "246") element.classList.add('diagonal'); 
+}
